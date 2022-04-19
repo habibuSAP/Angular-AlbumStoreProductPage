7117fe21975f8c2a7b63dcbed1f9e6d6ef5b38ea
Habibu <habibu.abdullahi@sap.com>
2022-04-19 12:36:35 +0200

    Add service to get album data

---
+++ src/app/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Http, Response} from '@angular/http';
+
+@Injectable()
+export class ProductService {
+  _albumUrl = '../assets/album.json';
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl);
+  }
+}
